253b581c1917afba8797a40c3b6bd415f375b611
AestheticCoder-rjp <160056441+AestheticCoder-rjp@users.noreply.github.com>
2024-06-11 13:12:23 +0530

    Update Diabetes_Detection.py

--- Diabetes_Detection.py
+++ Diabetes_Detection.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 from streamlit.components.v1 import custom_component as _components
 import pandas as pd
 from eda import display_eda
